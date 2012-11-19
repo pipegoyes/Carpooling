612984588c023a68a9017e1690e04eab41c8f79a
Pipegoyes <Pipegoyes@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-11-19 02:24:59 +0000

    Capa de negocios parada y viaje modificaciones

---
+++ Carpooling/BusinessLayer/Servicios/Transformador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BusinessLayer.BussinessObjects;
+using Entities;
+using DataLayer;
+namespace BusinessLayer.Servicios
+{
+    public class Transformador
+    {
+        public static void CrearViajeEntity(Viaje viajeActual)
+        {
+            try
+            {
+                VIAJE viajeDao = new VIAJE();
+                viajeDao.CREADOR = "PIPE";
+                //viajeDao.HORA_PARTIDA = viajeActual.HoraPartida;
+                //TODO no podria ser una band el vehiculo
+                viajeDao.CUPOS = Convert.ToByte(viajeActual.Cupos);
+                
+                //TODO esta fecha de creacion deberia ser con la hora y fecha del servidor
+                viajeDao.FECHA_CREACION = DateTime.Now;
+                viajeActual.FechaPartida = viajeActual.FechaPartida;
+
+                List<Parada> listParadas = new List<Parada>();
+                listParadas.Add(viajeActual.Origin);
+                listParadas.AddRange(viajeActual.Waypoints);
+                listParadas.Add(viajeActual.Destination);
+
+                List<TRAYECTO> listTrayectos = new List<TRAYECTO>();
+                for (int i = 0; i < listParadas.Count-1; i++)
+                {
+                    for (int j = i+1; j < listParadas.Count; j++)
+                    {
+                        
+                        TRAYECTO trayectoActual = new TRAYECTO();
+                        if (j-i ==1)
+                            trayectoActual.ES_SIMPLE = "S";
+                        else
+                            trayectoActual.ES_SIMPLE = "N";
+                        trayectoActual.INDEX_ORIGEN = i;
+                        trayectoActual.INDEX_DESTINO = j;
+                        
+                        CORDENADA coordenadaOrigen = new CORDENADA();
+                        coordenadaOrigen.LATITUD = listParadas[i].Latitud;
+                        coordenadaOrigen.LONGITUD = listParadas[i].Longitud;
+                        coordenadaOrigen.DIRECCION = listParadas[i].Direccion;
+                        GeneradorInserts.InsertarCoordenada(coordenadaOrigen);
+
+                        CORDENADA coordenadaDestino = new CORDENADA();
+                        coordenadaDestino.LATITUD = listParadas[j].Latitud;
+                        coordenadaDestino.LONGITUD = listParadas[j].Longitud;
+                        coordenadaDestino.DIRECCION = listParadas[j].Direccion;
+                        GeneradorInserts.InsertarCoordenada(coordenadaDestino);
+
+                        trayectoActual.COORDENADA_ORIGEN = ConsultorDB.ObtenerIdCoordenada(coordenadaOrigen);
+                        trayectoActual.COORDENADA_DESTINO = ConsultorDB.ObtenerIdCoordenada(coordenadaDestino);
+                        trayectoActual.CUPOS_DISPONIBLES = Convert.ToByte(viajeActual.Cupos);
+                        listTrayectos.Add(trayectoActual);
+                    }
+                }
+                foreach (TRAYECTO trayectoDao in listTrayectos)
+                    viajeDao.TRAYECTO.Add(trayectoDao);    
+                GeneradorInserts.InsertarViaje(viajeDao);
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+    }
+}
