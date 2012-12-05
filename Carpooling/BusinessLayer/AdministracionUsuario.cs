0ac751a0ecd31b67ae35ce3511aec4bedcb32807
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-12-05 01:13:08 +0000

    Consolidado dev

--- Carpooling/BusinessLayer/AdministracionUsuario.cs
+++ Carpooling/BusinessLayer/AdministracionUsuario.cs
@@ -9,21 +9,21 @@ namespace BusinessLayer
 {
     public static class AdministracionUsuario
     {
-        //Valida e inserta un nuevo usuario
-        public static void CrearUsuario(USUARIO pUsuario)
-        {
-            try
-            {
-                //Verifica que el usuario no exista
-                //TODO:
+        ////Valida e inserta un nuevo usuario
+        //public static void CrearUsuario(USUARIO pUsuario)
+        //{
+        //    try
+        //    {
+        //        //Verifica que el usuario no exista
+        //        //TODO:
 
-                //Envia el usuario para su insercion en la base de datos
-                UsuarioDL.InsertarUsuario(pUsuario);
-            }
-            catch (Exception ex)
-            {                    
-                throw ex;
-            }
-        }
+        //        //Envia el usuario para su insercion en la base de datos
+        //        UsuarioDL.InsertarUsuario(pUsuario);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
     }
 }
