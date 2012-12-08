8da52e3bc084eb7a3833e2ec960ecf0f44c6eca8
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-12-08 23:29:19 +0000

    Implementacion arquitectura DataLayer

--- DataLayer/AdministradorBD/GeneradorInserts.cs
+++ DataLayer/AdministradorBD/GeneradorInserts.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Entities;
-namespace DataLayer
+using DataLayer.ModeloEntityFramework.P;
+
+namespace DataLayer.AdministradorBD
 {
     public class GeneradorInserts
     {
