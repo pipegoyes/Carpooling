3f9e54a8d7f0daf3c664b46c63a367d88ae91451
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-12-08 23:29:19 +0000

    Implementacion arquitectura DataLayer

--- Carpooling/DataLayer/AdministradorBD/UsuarioDL.cs
+++ Carpooling/DataLayer/AdministradorBD/UsuarioDL.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Entities;
-namespace DataLayer
+
+namespace DataLayer.AdministradorBD
 {
     public static class UsuarioDL
     {
