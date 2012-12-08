db6cfa383730717e3512ed45e7b414d5e93de1c2
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-12-08 23:38:29 +0000

    Implementacion arquitectura Carpooling

---
+++ Carpooling/Front/Cuentas/Perfil3.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carpooling.Front.Cuentas
+{
+    public partial class Perfil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Carpooling/Front/Viajes/CrearViajePaso1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Services;
+using BusinessLayer.ServiciosViaje;
+using Entities.Negocio;
+
+namespace Carpooling.Front.Viajes
+{
+    public partial class CrearViajePaso1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //List<int> listHoras = Enumerable.Range(00, 24).ToList();
+            //List<int> listMinutos = Enumerable.Range(00, 59).ToList();
+            //this.comboBoxHour.DataSource = listHoras;
+            //this.comboBoxMinutos.DataSource = listMinutos;
+            //this.btnRolConductor.Checked
+        }
+
+        [WebMethod]
+        public static string PublicarViajeAsynch(Viaje request)
+        {
+            Transformador.CrearViajeEntity(request);
+
+            return "Origen del viaje - " + request.Tarifa; ;
+        }
+
+    }
+}
