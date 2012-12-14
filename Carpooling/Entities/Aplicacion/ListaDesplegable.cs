4629d152a07a0bc2d70c8d656080f065a70c246c
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2012-12-14 01:38:47 +0000

    Entities Dao Conflicto

---
+++ Carpooling/Entities/Aplicacion/ListaDesplegable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Aplicacion
+{
+    class ListaDesplegable
+    {
+        public string Valor { get; set; }
+        public string Dato { get; set; }
+    }
+}
