357c20c528091b0a0f02958d7c8ff9d84e2faa87
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-03-20 03:36:59 +0000

    conflito pecueco

---
+++ Entities/Aplicacion/ListaDesplegable2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Aplicacion
+{
+    public class ListaDesplegable2
+    {
+        public string Valor { get; set; }
+        public string Dato { get; set; }
+
+        public ListaDesplegable2()
+        {
+        }
+
+        public ListaDesplegable2(string pValor, string pDato)
+        {
+            Valor = pValor;
+            Dato = pDato;
+        }
+    } 
+}
