e2fe292a649864d6746445d3eddf9e63102a0d9d
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-03-24 22:29:20 +0000

    Nuevas entidades

---
+++ Carpooling/Entities/Negocio/Ocupacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Negocio
+{
+    public class Ocupacion
+    {
+        public int IdOcupacion { get; set; }
+        public string NombreOcupacion { get; set; }
+    }
+}
