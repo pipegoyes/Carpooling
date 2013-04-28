ffc3b353276e17bb24c838cf9355f85a59368a83
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-04-28 04:30:47 +0000

    Inclusion CS de enumeraciones

--- Carpooling/Entities/Negocio/Viaje.cs
+++ Carpooling/Entities/Negocio/Viaje.cs
@@ -7,14 +7,6 @@ namespace Entities.Negocio
 {
     public class Viaje
     {
-        [Flags]
-        public enum ViajeEstado : int
-        {
-            Publicado = 1,
-            Cancelado = 2,
-            Realizado = 3
-        }
-
         public int IdViaje { get; set; }
         public decimal AporteEconomico { get; set; }
         public Usuario Conductor { get; set; }
@@ -23,9 +15,6 @@ namespace Entities.Negocio
         public DateTime FechaCreacion { get; set; }
         public List<Trayecto> TrayectosViaje { get; set; }
         public List<Pregunta> Preguntas { get; set; }
-        public List<Calificacion> Calificaciones { get; set; } 
-        
-        //public bool EsConductor { get; set; }
-        //public string HoraPartida { get; set; }        
+        public List<Calificacion> Calificaciones { get; set; }      
     }
 }
