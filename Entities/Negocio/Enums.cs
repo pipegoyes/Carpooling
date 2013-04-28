508ebe21205776f4062d7019daff28a5b6c93b27
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-04-28 04:30:47 +0000

    Inclusion CS de enumeraciones

---
+++ Entities/Negocio/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities.Negocio
+{
+    [Flags]
+    public enum ViajeEstado : int
+    {
+        Publicado = 1,
+        Cancelado = 2,
+        Realizado = 3
+    }
+}
