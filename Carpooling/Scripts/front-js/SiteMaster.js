f486f394d0074cbbdeb025b6f6fefbddaaff3ad5
manuellinares007 <manuellinares007@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-04-29 04:53:35 +0000

    cambios master menu

--- Carpooling/Scripts/front-js/SiteMaster.js
+++ Carpooling/Scripts/front-js/SiteMaster.js
@@ -1,19 +1,4 @@
-﻿$(document).ready(function () {
-    //Evento click boton inicio sesion
-    $("[ClientID='btnIniciarSesion']").click(function () {
-        $('#hfdNombreUsuario').val($('#txbNombreUsuario').val());
-        $('#hfdContrasenia').val($('#txbContrasenia').val());
-    });
-});
-
-//para resuscribir los eventos de los controles dentro de updatepanel
-pageRequestManager.add_endRequest(function () {
-    $("[ClientID='btnIniciarSesion']").click(function () {
-        $('#hfdNombreUsuario').val($('#txbNombreUsuario').val());
-        $('#hfdContrasenia').val($('#txbContrasenia').val());
-    });
-});
-
+﻿/**********************************FUNCIONES JS***********************************/
 //funcion para mostrar el formulario de login
 function lnkIniciaSesion_OnClientClick() {
     var lnkIniciaSesion = $('#lnkIniciaSesion');
@@ -24,6 +9,7 @@ function lnkIniciaSesion_OnClientClick() {
     $('#divLoginFormFloat').css('left', leftDivLogin);
     $('#divLoginFormFloat').css('top', topDivLogin);
     $('#divLoginFormFloat').toggle('blind', 500);
+    return false;
 }
 
 //funcion para cambiar los valores de los hidden fields de nombre de usuario y contraseña
@@ -31,3 +17,20 @@ function btnIniciarSesion_OnClientClick() {
     $('#hfdNombreUsuario').val($('#txbNombreUsuario').val());
     $('#hfdContrasenia').val($('#txbContrasenia').val());
 }
+
+
+/*******************************EVENTOS JQUERY**********************************/
+//evento al terminar la carga del documento
+$(document).ready(function () {
+    $("[ClientID='btnIniciarSesion']").click(function () {
+        btnIniciarSesion_OnClientClick();
+    });
+
+    $('#lnkIniciaSesion').click(function () {
+        lnkIniciaSesion_OnClientClick();
+    });
+});
+
+//$(window).load(function () {
+
+//});
