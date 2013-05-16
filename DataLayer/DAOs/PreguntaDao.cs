8fd936c590ebd0937db1aca2491dbc7d4d14442c
Pipegoyes <Pipegoyes@66358cd0-a2d6-4cf4-b29a-643968248539>
2013-05-16 03:33:48 +0000

    Actualizacion de la respuesta de una pregunta (solo la parte del DAO)

---
+++ DataLayer/DAOs/PreguntaDao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entities.Negocio;
+using DataLayer.Transformador;
+
+namespace DataLayer.DAOs
+{
+    public class PreguntaDao : BaseDao
+    {
+        private static PreguntaDao _instancia;
+
+        private PreguntaDao()
+        {
+        }
+
+        public static PreguntaDao Instancia
+        {
+            get { return _instancia ?? (_instancia = new PreguntaDao()); }
+        }
+
+        public bool ActualizarPregunta(Pregunta pPregunta)
+        {
+            EstablecerConexion();
+            var preguntaDb = ToDataEntity.Instancia.ToPregunta(pPregunta);
+            Conexion.PREGUNTA.Attach(preguntaDb);
+            var entidad = Conexion.Entry(preguntaDb);
+            entidad.Property(p => p.TEXTO_RESPUESTA).IsModified = true;
+            return ConfirmarCambios();
+        }
+    }
+}
