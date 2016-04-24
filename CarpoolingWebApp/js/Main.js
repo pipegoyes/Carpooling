1c8d2ed9e090f4d2e6ae3a820bf9c349646bf9bc
pipegoyes <pipegoyes@gmail.com>
2016-04-24 12:05:08 +0200

    merge to master from pages

---
+++ CarpoolingWebApp/js/Main.js
@@ -0,0 +1,13 @@
+﻿// some directives
+// ng-show, ng-hide mit {{
+// ng - repeat ng-repeat="product in store.products"
+
+(function () {
+    var app = angular.module("mainModule", []);
+    app.controller("mainController", function () {
+        this.product = JSON;
+    });
+
+})();
+
+    
